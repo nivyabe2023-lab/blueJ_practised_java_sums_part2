5b3477fed914f2a7a0206ae0461686082b93cb48
nivyabe2023-lab <nivyabe2023@gmail.com>
2026-03-21 23:08:58 +0530

    main

---
+++ ShoppingSystem.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+class product{
+    public int product_id;
+    public String product_name;
+    public int product_price;
+    public String product_category;
+    
+    public product(int product_id,String product_name,int product_price,String product_category){
+         this.product_id = product_id;
+         this.product_name = product_name;
+         this.product_price = product_price;
+         this.product_category = product_category;
+    }
+}
+
+class cartitem{
+    
+}
+
+class cart{
+    
+    
+}
+
+class user{
+    
+}
+
+class discount{
+    
+}
+
+class store{
+    
+    
+}
+public class ShoppingSystem{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        ArrayList <product> products = new ArrayList<>();
+        
+        
+    }
+}
