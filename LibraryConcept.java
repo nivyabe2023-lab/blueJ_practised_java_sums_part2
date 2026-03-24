47a2c153cc7a9a29b15578e3f2d19b0e669897a5
nivyabe2023-lab <nivyabe2023@gmail.com>
2026-03-24 23:08:10 +0530

    LibraryConcept

---
+++ LibraryConcept.java
@@ -0,0 +1,24 @@
+class Book {
+    public String book_name;
+    public int book_id;
+    public String book_issued_date;
+
+    public Book(String book_name, int book_id, String book_issued_date){
+        
+    }
+}
+
+class Student {
+    public String student_name;
+    public int student_id;
+    public String student_department;
+
+    public Student(String student_name, int student_id, String student_department){
+        
+    }
+}
+
+public class LibraryConcept {
+    public static void main(String[] args){
+    }
+}
